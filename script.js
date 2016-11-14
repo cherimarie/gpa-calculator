af5e00ffaf7dc125d78ca0ae487426e92d4a403b
Cheri Allen <cherimarie@gmail.com>
2016-11-13 17:57:09 -0800

    Cleaner js comments and variable naming

--- script.js
+++ script.js
@@ -1,83 +1,98 @@
 /*
-  This web application helps a user calculate a GPA for their classes. They enter a name and grade for each class. We build a list of these classes, and print it to the page every time it changes.
+  This web application helps a user calculate a GPA for their courses.
 
-  When user clicks the "calculate" button, we get the average of the grades given for each class, and print it to the page.
+  User enters a name and grade for each course via a form. We build a list of these courses,
+  and print it to the page every time it changes (on form submit).
 
-  When user clicks "clear" button, the list of classes is emptied.
+  When user clicks the "calculate" button, we get the average of the grades for each
+  course in the list, and print it to the page.
 
-  build constructor- can take list of args or object literal
-  onclic event for form submit
-    validate data - use typeof for nubers
-    build new object
-    put in array
-    reprint array to page
-  calculate button
-    runs func on all objects to get avg
-    prints avg to page
-  add/remove classes buttons?
+  When user clicks "clear" button, the list of courses is emptied.
+  The page is updated to remove the old list and any previously calculated GPA.
 */
 
-function NewClass(name, grade){
+// Constructor to build new course objects.
+function Course(name, grade){
   this.name = name;
   this.grade = grade;
 }
 
-var classList = [];
+// Global variable to hold list of courses.
+var courseList = [];
 
+// Assign functions to submission of the course form, click of two buttons.
 window.onload = function(){
-  document.getElementById('class-form').onsubmit = formHandler;
+  document.getElementById("course-form").onsubmit = formHandler;
 
   document.getElementById("clear").onclick = clearList;
 
   document.getElementById("calculate").onclick = calculateAverage;
 }
 
+// Triggered on form submit. Creates a new course object and pushes it into courseList array; clears content in form fields; prints courseList objects to the page.
+function formHandler(){
+  var grade = parseFloat(this.elements["grade"].value);
+  // should courseList really be maintained here? and return functions that can be used to add to or clear list?
+
+  /*TODO validate that "grade" value is a number between 1 and 4.
+
+    Checking 'grade typeof "number"' will always return true because we called parseFloat.
+    We must check that it's value is not NaN.
+
+    Or is there a way we could use HTML to do it?
+    http://w3schools.pro.edu/html/html_form_input_types.asp
+  */
+  courseList.push(new Course(this.elements["course-name"].value, grade));
+
+  clearFormFields();
+  outputList();
+
+  // Prevent form from trying to submit to a server.
+  return false;
+}
+
+// Triggered by clicking "clear". Empties global courseList array, removes list items and GPA calculation from page text.
 function clearList(){
-  classList = [];
+  courseList = [];
   outputList();
   clearGPA();
 }
 
+// Calculate the average of "grade" attribute for each object in courseList array and prints average in friendly message to page text.
+function calculateAverage(){
+  var sum = 0;
+  for(i=0;i<courseList.length;i++){
+    sum += courseList[i].grade;
+  }
+
+  var avg = sum / courseList.length;
+  document.getElementById("result").innerHTML = "Your overall GPA is " + avg;
+}
+
+// Removes GPA calculation from page text.
 function clearGPA(){
   document.getElementById("result").innerHTML = null;
 }
 
-function calculateAverage(){
-  var sum = 0;
-  for(i=0;i<classList.length;i++){
-    sum += classList[i].grade;
-  }
-
-  var avg = sum / classList.length;
-  document.getElementById("result").innerHTML = "Your overall GPA is " + avg;
-}
-
-function formHandler(){
-  var grade = parseFloat(this.elements["grade"].value);
-  // should classList really be maintained here? and return functions that can be used to add to or clear list?
-  //TODO check that grade !== NaN; checking typeof "number" useless because we set it to that. Also that it's value is between 0-4.
-  classList.push(new NewClass(this.elements["class-name"].value, grade));
-  clearFormFields();
-  outputList();
-  return false;
-}
-
+// Clears content in form fields.
 function clearFormFields(){
-  var classForm = document.getElementById('class-form');
-  classForm.elements["class-name"].value = "";
-  classForm.elements["grade"].value = "";
+  var courseForm = document.getElementById('course-form');
+
+  courseForm.elements["course-name"].value = "";
+  courseForm.elements["grade"].value = "";
 }
 
+// Prints courseList objects to the page in a readable way.
 function outputList(){
-  var list = document.getElementById("class-list");
+  var list = document.getElementById("course-list");
   list.innerHTML = null;
 
-  for (i = 0; i < classList.length; i++) {
+  for (i = 0; i < courseList.length; i++) {
     // Create an <li> node
     var node = document.createElement("LI");
 
     // Create a text node
-    var textnode = document.createTextNode(classList[i].name + " - " + classList[i].grade);
+    var textnode = document.createTextNode(courseList[i].name + " - " + courseList[i].grade);
 
     // Append/add the text to <li>
     node.appendChild(textnode);
