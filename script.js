fcedd0c2c24d1a68d7b3b5407e650b9849455927
Cheri Allen <cherimarie@gmail.com>
2016-11-13 20:15:19 -0800

    Transform js file into assignment

--- script.js
+++ script.js
@@ -2,39 +2,44 @@
   This web application helps a user calculate a GPA for their courses.
 
   User enters a name and grade for each course via a form. We build a list of these courses,
-  and print it to the page every time it changes (on form submit).
+  and print the list to the page every time it changes (on form submit).
 
   When user clicks the "calculate" button, we get the average of the grades for each
   course in the list, and print it to the page.
 
   When user clicks "clear" button, the list of courses is emptied.
   The page is updated to remove the old list and any previously calculated GPA.
+
+  Comments labeled TODO and BUGFIX must be done for the application to work properly.
+  REVIEW comments are requests for you to use your judgement to optimize the code.
 */
 
-// Constructor to build new course objects.
-function Course(name, grade){
-  this.name = name;
-  this.grade = grade;
-}
+
+// TODO: Write a constructor called "Course" to create objects with "name" and "grade" attributes.
 
 // Global variable to hold list of courses.
 var courseList = [];
+// REVIEW: Would this be safer if variable was scoped to window.onload or formHandler functions?
 
-// Assign functions to submission of the course form, click of two buttons.
+// Assign handlers to page events.
 window.onload = function(){
   document.getElementById("course-form").onsubmit = formHandler;
 
-  document.getElementById("clear").onclick = clearList;
+  // TODO: Assign handler for click of "clear" button. It should empty courseList array,
+  // remove list items and GPA calculation from page text.
 
   document.getElementById("calculate").onclick = calculateAverage;
+  // REVIEW: Would it make more sense to make some handlers anonymous functions,
+  // instead of defining them elsewhere and assigning them here?
 }
 
-// Triggered on form submit. Creates a new course object and pushes it into courseList array; clears content in form fields; prints courseList objects to the page.
+// Triggered on form submit. Creates a new course object and pushes it into courseList array,
+// clears content in form fields, prints courseList objects to the page.
 function formHandler(){
   var grade = parseFloat(this.elements["grade"].value);
-  // should courseList really be maintained here? and return functions that can be used to add to or clear list?
 
-  /*TODO validate that "grade" value is a number between 1 and 4.
+  /*
+    TODO: validate that "grade" value is a number between 1.0 and 4.0.
 
     Checking 'grade typeof "number"' will always return true because we called parseFloat.
     We must check that it's value is not NaN.
@@ -42,7 +47,11 @@ function formHandler(){
     Or is there a way we could use HTML to do it?
     http://w3schools.pro.edu/html/html_form_input_types.asp
   */
-  courseList.push(new Course(this.elements["course-name"].value, grade));
+
+  // Create the new course with values given, push it into array of courses.
+  // BUGFIX: this causes an error because the "Course" constructor is not yet defined.
+  var newCourse = new Course(this.elements["course-name"].value, grade);
+  courseList.push(newCourse);
 
   clearFormFields();
   outputList();
@@ -51,21 +60,12 @@ function formHandler(){
   return false;
 }
 
-// Triggered by clicking "clear". Empties global courseList array, removes list items and GPA calculation from page text.
-function clearList(){
-  courseList = [];
-  outputList();
-  clearGPA();
-}
-
-// Calculate the average of "grade" attribute for each object in courseList array and prints average in friendly message to page text.
+// Calculate the average of "grade" attribute for all objects in courseList array
+// and prints it in friendly message to page text.
 function calculateAverage(){
-  var sum = 0;
-  for(i=0;i<courseList.length;i++){
-    sum += courseList[i].grade;
-  }
-
-  var avg = sum / courseList.length;
+  // TODO: Calculate the average GPA of courses (the sum of all grades,
+  // divided by the number of course objects) and assign that value to "avg" variable.
+  var avg = "";
   document.getElementById("result").innerHTML = "Your overall GPA is " + avg;
 }
 
@@ -76,28 +76,16 @@ function clearGPA(){
 
 // Clears content in form fields.
 function clearFormFields(){
-  var courseForm = document.getElementById('course-form');
-
-  courseForm.elements["course-name"].value = "";
-  courseForm.elements["grade"].value = "";
+  // TODO: set the value of both the form fields to an empty string or null.
 }
 
 // Prints courseList objects to the page in a readable way.
 function outputList(){
   var list = document.getElementById("course-list");
-  list.innerHTML = null;
 
-  for (i = 0; i < courseList.length; i++) {
-    // Create an <li> node
-    var node = document.createElement("LI");
-
-    // Create a text node
-    var textnode = document.createTextNode(courseList[i].name + " - " + courseList[i].grade);
-
-    // Append/add the text to <li>
-    node.appendChild(textnode);
-
-    // Append/add the <li> to the selected <ul> element
-    list.appendChild(node);
-  }
+  /*
+    TODO: Clear the contents of the "list" element. For each course in courseList,
+    create an li element that holds the course's name and grade, then append
+    the li to the "list" ul element (this is the same process used in event-listings project).
+  */
 }
